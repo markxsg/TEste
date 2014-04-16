9ec37e1c81e745fc9b2fc37b495c21d0c4be44d2
Markxsg <markxsg.dev@gmail.com>
2014-04-16 10:12:09 +0900

    Hello World!

---
+++ CalculatorTutorial/src/net/nervis/Main.java
@@ -0,0 +1,17 @@
+package net.nervis;
+
+import javax.swing.UIManager;
+
+public class Main {
+	public static void main(String[]args) {
+		
+		try {
+			UIManager.setLookAndFeel("com.sun.java.swing.plaf.windows.WindowsLookAndFeel");
+		} catch (Exception e) {
+
+		}
+		
+		new Calculate();
+	}
+
+}
